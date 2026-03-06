d9902e4b0195cb4b4c847562c8df79eaccbf174c
zhaoy <zhaoyanzy@sjtu.edu.cn>
2026-03-06 12:25:50 +0000

    first commit

--- run.sh
+++
@@ -1,7 +0,0 @@
-nohup python train.py \
-        --unify --switch --moe --amp \
-        --name omni-switch-moe-xs-layer1-expert4-k2-mlp4 \
-        --model_name rwkv7_hira_switch_moe --model_size xs \
-        --pretrain_model checkpoints/omnicomp/omni_xs_layer1_expert4_k2_mlp4.pth \
-        --gpu_ids 2 --batch_size 64 --nepochs 16 \
-        > ./logs/omni-switch-moe-xs-layer1-expert4-k2-mlp4.log
